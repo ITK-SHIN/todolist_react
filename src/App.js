c2f1952cd2748fc8b57387d5cca7b2498039935e
신상우 <bano112@naver.com>
2023-08-05 22:26:56 +0900

    TodoInsert 만들기 완료

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
+import TodoInsert from './components/TodoInsert';
 import TodoTemplate from './components/TodoTemplate';
 
-const App = () => <TodoTemplate> Todo App을 만들자</TodoTemplate>;
+const App = () => (
+  <TodoTemplate>
+    <TodoInsert />
+  </TodoTemplate>
+);
 
 export default App;

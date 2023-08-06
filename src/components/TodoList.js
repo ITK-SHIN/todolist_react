853b3cd0f4ea4bb87cf083f82fe506de01d8f93a
신상우 <bano112@naver.com>
2023-08-06 19:56:04 +0900

    수정 및 삭제 기능 구현완료

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,10 +2,10 @@
 import './TodoList.scss';
 import TodoListItem from './TodoListItem';
 
-const TodoList = ({ todos }) => (
+const TodoList = ({ todos, onRemove, onToggle }) => (
   <div className="TodoList">
     {todos.map((todo) => (
-      <TodoListItem todo={todo} key={todo.id} />
+      <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle} />
     ))}
   </div>
 );
